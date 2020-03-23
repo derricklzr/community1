0f7133b78d010891f4fd957a35cbb54a16152651
Luo <1021848661@qq.com>
2020-03-23 22:17:40 +0800

    6.4搜索引擎测试完成

--- src/main/java/com/nowcoder/community/entity/DiscussPost.java
+++ src/main/java/com/nowcoder/community/entity/DiscussPost.java
@@ -1,20 +1,34 @@
 package com.nowcoder.community.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
 import java.util.Date;
 
+//将实体的数据和Es的索引等对应
+@Document(indexName = "discusspost",type = "_doc",shards = 6,replicas = 3)
 public class DiscussPost {
 
-
+    @Id//id字段
     private int id;
+    @Field(type = FieldType.Integer)//类型为Integer的普通字段
     private int userId;
+    //类型为Text的普通字段，存储分词器ik_max_word(把存入的词分成多个字段存储)，搜索分词器用smart
+    @Field(type = FieldType.Text,analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
     private String title;
-
-
+    @Field(type = FieldType.Text,analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
     private String content;
+    @Field(type = FieldType.Integer)
     private int type;
+    @Field(type = FieldType.Integer)
     private int status;
+    @Field(type = FieldType.Date)
     private Date createTime;
+    @Field(type = FieldType.Integer)
     private int commentCount;
+    @Field(type = FieldType.Double)
     private int score;
 
 
